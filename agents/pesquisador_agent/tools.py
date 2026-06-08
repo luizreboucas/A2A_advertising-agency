65fae885cde47507fe865815a67a1f6a8138196e
Luiz Rebouças Neto <luiz.neto@gfxconsultoria.com>
2026-06-08 11:41:13 -0300

    ajuste na pesquisa

--- agents/pesquisador_agent/tools.py
+++ agents/pesquisador_agent/tools.py
@@ -2,12 +2,14 @@ from tavily import TavilyClient
 from dotenv import load_dotenv
 from langchain.tools import tool
 import os
+import requests
+from ddgs.ddgs import DDGS
+from bs4 import BeautifulSoup
 
-load_dotenv()
-TAVILY_KEY=os.getenv("TAVILY_KEY")
+# load_dotenv()
+# TAVILY_KEY=os.getenv("TAVILY_KEY")
 
-client = TavilyClient(api_key=TAVILY_KEY)
-
+# client = TavilyClient(api_key=TAVILY_KEY)
 
 @tool
 def search_web(theme: str):
@@ -16,6 +18,19 @@ def search_web(theme: str):
         @args
             theme: tema a ser pesquisado
     """
-    result = client.search(query=theme)
-    return result
+    client = DDGS()
+    results = client.text(theme)
+    responses = []
+    for result in results[:5]:
+        url = result["href"]
+        response = requests.get(url)
+        if response.status_code == 200:
+            html = response.text
+            text = BeautifulSoup(html, "html.parser").get_text()
+            clean_text = " ".join(text.split())
+            responses.append(clean_text[:400])
+        else:
+            continue
+    final = "\n".join(response for response in responses)
+    return final
 
